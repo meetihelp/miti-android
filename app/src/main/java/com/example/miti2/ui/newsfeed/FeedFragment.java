d9303618db5ee8a8b9ba7fa2955946388383ea7a
rava-dosa <apoorvakumar169@gmail.com>
2019-12-09 14:10:23 +0530

    added otp screen and newsfeed card also implemented checkable

--- app/src/main/java/com/example/miti2/ui/newsfeed/FeedFragment.java
+++ app/src/main/java/com/example/miti2/ui/newsfeed/FeedFragment.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+//import androidx.cardview.widget.CardView;
+import com.google.android.material.card.MaterialCardView;
+
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -67,7 +70,19 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View v=inflater.inflate(R.layout.fragment_feed, container, false);
+        MaterialCardView card=v.findViewById(R.id.card_view);
+        card.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // TODO Auto-generated method stub
+                MaterialCardView card=v.findViewById(R.id.card_view);
+                card.setChecked(!card.isChecked());
+                return true;
+            }
+        });
+        return v;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
